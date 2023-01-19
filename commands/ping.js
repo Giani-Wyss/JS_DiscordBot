3349a84c494e0473fef82460d2b20df1fde96956
Dines Nimalthas <Dinesnimal@gmail.com>
2023-01-19 11:33:22 +0100

    Changes: ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
-};
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
+	}
+});
