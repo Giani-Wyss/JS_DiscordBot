1ba857a5d0073288a69b2be5a553f07d4afa3c99
Dines Nimalthas <Dinesnimal@gmail.com>
2023-01-19 10:40:55 +0100

    ping command added

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+
+client.commands = new Collection();
+
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
